0c4b79bff4fe4477bb7960da9a3b7928f7b8e045
Fabien Andrieux <fandrieu@cisco.com>
2016-11-02 17:04:20 +0100

    adding baremetal directory

--- baremetal/kvm_helpers/create_json.sh
+++ baremetal/kvm_helpers/create_json.sh
@@ -77,6 +77,121 @@ jq . << EOF > ~/instackenv.json
       "disk": "10",
       "arch": "x86_64",
       "pm_user": "stack"
+<<<<<<< HEAD
+=======
+    },
+    {
+      "name": "$(sed -n 6p /tmp/names.txt)",
+      "pm_addr": "192.168.122.1",
+      "pm_password": "$(cat ~/.ssh/id_rsa)",
+      "pm_type": "pxe_ssh",
+      "mac": [
+        "$(sed -n 6p /tmp/nodes.txt)"
+      ],
+      "cpu": "1",
+      "memory": "1024",
+      "disk": "10",
+      "arch": "x86_64",
+      "pm_user": "stack"
+    },
+    {
+      "name": "$(sed -n 7p /tmp/names.txt)",
+      "pm_addr": "192.168.122.1",
+      "pm_password": "$(cat ~/.ssh/id_rsa)",
+      "pm_type": "pxe_ssh",
+      "mac": [
+        "$(sed -n 7p /tmp/nodes.txt)"
+      ],
+      "cpu": "1",
+      "memory": "1024",
+      "disk": "10",
+      "arch": "x86_64",
+      "pm_user": "stack"
+    },
+    {
+      "name": "$(sed -n 8p /tmp/names.txt)",
+      "pm_addr": "192.168.122.1",
+      "pm_password": "$(cat ~/.ssh/id_rsa)",
+      "pm_type": "pxe_ssh",
+      "mac": [
+        "$(sed -n 8p /tmp/nodes.txt)"
+      ],
+      "cpu": "1",
+      "memory": "1024",
+      "disk": "10",
+      "arch": "x86_64",
+      "pm_user": "stack"
+    },
+    {
+      "name": "$(sed -n 9p /tmp/names.txt)",
+      "pm_addr": "192.168.122.1",
+      "pm_password": "$(cat ~/.ssh/id_rsa)",
+      "pm_type": "pxe_ssh",
+      "mac": [
+        "$(sed -n 9p /tmp/nodes.txt)"
+      ],
+      "cpu": "1",
+      "memory": "1024",
+      "disk": "10",
+      "arch": "x86_64",
+      "pm_user": "stack"
+    },
+    {
+      "name": "$(sed -n 10p /tmp/names.txt)",
+      "pm_addr": "192.168.122.1",
+      "pm_password": "$(cat ~/.ssh/id_rsa)",
+      "pm_type": "pxe_ssh",
+      "mac": [
+        "$(sed -n 10p /tmp/nodes.txt)"
+      ],
+      "cpu": "1",
+      "memory": "1024",
+      "disk": "10",
+      "arch": "x86_64",
+      "pm_user": "stack"
+    },
+    {
+      "name": "$(sed -n 11p /tmp/names.txt)",
+      "pm_addr": "192.168.122.1",
+      "pm_password": "$(cat ~/.ssh/id_rsa)",
+      "pm_type": "pxe_ssh",
+      "mac": [
+        "$(sed -n 11p /tmp/nodes.txt)"
+      ],
+      "cpu": "1",
+      "memory": "1024",
+      "disk": "10",
+      "arch": "x86_64",
+      "pm_user": "stack"
+    },
+    {
+      "name": "$(sed -n 12p /tmp/names.txt)",
+      "pm_addr": "192.168.122.1",
+      "pm_password": "$(cat ~/.ssh/id_rsa)",
+      "pm_type": "pxe_ssh",
+      "mac": [
+        "$(sed -n 12p /tmp/nodes.txt)"
+      ],
+      "cpu": "1",
+      "memory": "1024",
+      "disk": "10",
+      "arch": "x86_64",
+      "pm_user": "stack"
+    },
+    {
+      "name": "$(sed -n 13p /tmp/names.txt)",
+      "pm_addr": "192.168.122.1",
+      "pm_password": "$(cat ~/.ssh/id_rsa)",
+      "pm_type": "pxe_ssh",
+      "mac": [
+        "$(sed -n 13p /tmp/nodes.txt)"
+      ],
+      "cpu": "1",
+      "memory": "1024",
+      "disk": "10",
+      "arch": "x86_64",
+      "pm_user": "stack"
+>>>>>>> adding baremetal directory
     }
   ]
 }
--- baremetal/kvm_helpers/create_vms.sh
+++ baremetal/kvm_helpers/create_vms.sh
@@ -4,6 +4,7 @@ set -xe
 
 for i in $(virsh list --all | awk ' /pnda/ {print $2} '); do virsh undefine $i;done
 
+<<<<<<< HEAD
 # SALTMASTER
 qemu-img create -f qcow2 -o preallocation=metadata pnda-master.qcow2 10G
 virt-install --accelerate --cpu SandyBridge,+vmx --name=pnda-master --file=pnda-master.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-master.xml
@@ -40,6 +41,57 @@ qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-mgr$i.qcow2 40G;\
 done
 for i in {1..1};do \
 virt-install --accelerate --cpu SandyBridge,+vmx --name=pnda-cdh-mgr$i --file=pnda-cdh-mgr$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-mgr$i.xml; \
+=======
+qemu-img create -f qcow2 -o preallocation=metadata pnda-master.qcow2 10G
+virt-install --accelerate --name=pnda-master --file=pnda-master.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-master.xml
+virsh define pnda-master.xml
+
+for i in {1..3};do qemu-img create -f qcow2 -o preallocation=metadata pnda-zookeeper-$i.qcow2 20G;done
+for i in {1..3};do virt-install --accelerate --name=pnda-zookeeper-$i --file=pnda-zookeeper-$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-zookeeper-$i.xml;done
+for i in {1..3};do virsh define pnda-zookeeper-$i.xml; done
+
+for i in {1..2};do qemu-img create -f qcow2 -o preallocation=metadata pnda-kafka-$i.qcow2 40G;done
+for i in {1..2};do virt-install --accelerate --name=pnda-kafka-$i --file=pnda-kafka-$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=4096 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-kafka-$i.xml;done
+for i in {1..2};do virsh define pnda-kafka-$i.xml; done
+
+#qemu-img create -f qcow2 -o preallocation=metadata pnda-tools.qcow2 10G
+#virt-install --accelerate --name=pnda-tools --file=pnda-tools.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch  --network network=default --os-type=linux --boot hd --dry-run --print-xml > pnda-tools.xml
+#virsh define pnda-tools.xml
+
+qemu-img create -f qcow2 -o preallocation=metadata pnda-logserver.qcow2 40G
+virt-install --accelerate --name=pnda-logserver --file=pnda-logserver.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=32768 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-logserver.xml
+virsh define pnda-logserver.xml
+
+#qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-edge.qcow2 20G
+#virt-install --accelerate --name=pnda-cdh-edge --file=pnda-cdh-edge.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch  --network network=default --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-edge.xml
+#virsh define pnda-cdh-edge.xml
+
+qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-cm.qcow2 40G
+virt-install --accelerate --name=pnda-cdh-cm --file=pnda-cdh-cm.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=32768 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-cm.xml
+virsh define pnda-cdh-cm.xml
+
+#qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-jupyter.qcow2 40G
+#virt-install --accelerate --name=pnda-cdh-jupyter --file=pnda-cdh-jupyter.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=4096 --network bridge=br-ctlplane,virtualport_type=openvswitch  --network network=default --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-jupyter.xml
+#virsh define pnda-cdh-jupyter.xml
+
+#qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-opentsdb.qcow2 40G
+#virt-install --accelerate --name=pnda-cdh-opentsdb --file=pnda-cdh-opentsdb.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=4096 --network bridge=br-ctlplane,virtualport_type=openvswitch  --network network=default --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-opentsdb.xml
+#virsh define pnda-cdh-opentsdb.xml
+
+for i in {1..3};do \
+qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-dn$i.qcow2 40G;\
+done
+for i in {1..3};do \
+virt-install --accelerate --name=pnda-cdh-dn$i --file=pnda-cdh-dn$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=8192 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-dn$i.xml; \
+virsh define pnda-cdh-dn$i.xml; \
+done
+
+for i in {1..2};do \
+qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-mgr$i.qcow2 40G;\
+done
+for i in {1..2};do \
+virt-install --accelerate --name=pnda-cdh-mgr$i --file=pnda-cdh-mgr$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=32768 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-mgr$i.xml; \
+>>>>>>> adding baremetal directory
 virsh define pnda-cdh-mgr$i.xml; \
 done
 
