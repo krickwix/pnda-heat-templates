8fb391ef9340fc2d4cafc685e9b8daefdd041e33
Fabien Andrieux <krickwix@gmail.com>
2016-11-04 07:07:42 -0400

    kvm helpers for pico

--- baremetal/kvm_helpers/create_flavors.sh
+++ baremetal/kvm_helpers/create_flavors.sh
@@ -3,31 +3,22 @@
 #set -xe
 
 
-openstack flavor delete p.master
-openstack flavor delete p.tools
-openstack flavor delete p.zookeeper
-openstack flavor delete p.kafka
-openstack flavor delete p.logserver
-openstack flavor delete p.edge
-openstack flavor delete p.cm
-openstack flavor delete p.jupyter
-openstack flavor delete p.dn
-openstack flavor delete p.mgr
-openstack flavor delete p.opentsdb
-openstack flavor create --id auto --ram 2048 --disk 9 --vcpus 1 p.master
-openstack flavor create --id auto --ram 2048 --disk 19 --vcpus 1 p.zookeeper
-openstack flavor create --id auto --ram 4096 --disk 39 --vcpus 2 p.kafka
-openstack flavor create --id auto --ram 32768 --disk 39 --vcpus 2 p.logserver
-openstack flavor create --id auto --ram 2048 --disk 19 --vcpus 1 p.edge
-openstack flavor create --id auto --ram 32768 --disk 39 --vcpus 2 p.cm
-openstack flavor create --id auto --ram 8192 --disk 39 --vcpus 2 p.dn
-openstack flavor create --id auto --ram 32768 --disk 39 --vcpus 2 p.mgr
-openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="master" p.master
-openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="zookeeper" p.zookeeper
-openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="kafka" p.kafka
-openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="logserver" p.logserver
-openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="edge" p.edge
-openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="cm" p.cm
-openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="dn" p.dn
-openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="mgr" p.mgr
+openstack flavor delete pnda-micro
+openstack flavor delete pnda-zookeeper
+openstack flavor delete pico-kafka
+openstack flavor delete pico-edge
+openstack flavor delete pico-dn
+openstack flavor delete pico-mgr
+openstack flavor create --id auto --ram 2048 --disk 9 --vcpus 1 pnda-micro
+openstack flavor create --id auto --ram 2048 --disk 19 --vcpus 1 pnda-zookeeper
+openstack flavor create --id auto --ram 4096 --disk 39 --vcpus 2 pico-kafka
+openstack flavor create --id auto --ram 2048 --disk 19 --vcpus 1 pico-edge
+openstack flavor create --id auto --ram 8192 --disk 39 --vcpus 2 pico-dn
+openstack flavor create --id auto --ram 32768 --disk 39 --vcpus 2 pico-mgr
+openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="master" pnda-micro
+openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="zookeeper" pnda-zookeeper
+openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="kafka" pico-kafka
+openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="edge" pico-edge
+openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="dn" pico-dn
+openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="mgr" pico-mgr
 
--- baremetal/kvm_helpers/create_vms.sh
+++ baremetal/kvm_helpers/create_vms.sh
@@ -10,35 +10,35 @@ virt-install --accelerate --name=pnda-master --file=pnda-master.qcow2 --graphics
 virsh define pnda-master.xml
 
 # ZOOKEPER
-for i in {1..1};do qemu-img create -f qcow2 -o preallocation=metadata pnda-zookeeper-$i.qcow2 20G;done
-for i in {1..1};do virt-install --accelerate --name=pnda-zookeeper-$i --file=pnda-zookeeper-$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-zookeeper-$i.xml;done
-for i in {1..1};do virsh define pnda-zookeeper-$i.xml; done
+#for i in {1..1};do qemu-img create -f qcow2 -o preallocation=metadata pnda-zookeeper-$i.qcow2 20G;done
+#for i in {1..1};do virt-install --accelerate --name=pnda-zookeeper-$i --file=pnda-zookeeper-$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-zookeeper-$i.xml;done
+#for i in {1..1};do virsh define pnda-zookeeper-$i.xml; done
 
 # KAFKA
-for i in {1..2};do qemu-img create -f qcow2 -o preallocation=metadata pnda-kafka-$i.qcow2 40G;done
-for i in {1..2};do virt-install --accelerate --name=pnda-kafka-$i --file=pnda-kafka-$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=4096 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-kafka-$i.xml;done
-for i in {1..2};do virsh define pnda-kafka-$i.xml; done
+for i in {1..1};do qemu-img create -f qcow2 -o preallocation=metadata pnda-kafka-$i.qcow2 40G;done
+for i in {1..1};do virt-install --accelerate --name=pnda-kafka-$i --file=pnda-kafka-$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=4096 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-kafka-$i.xml;done
+for i in {1..1};do virsh define pnda-kafka-$i.xml; done
 
 qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-edge.qcow2 20G
 virt-install --accelerate --name=pnda-cdh-edge --file=pnda-cdh-edge.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch  --network network=default --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-edge.xml
 virsh define pnda-cdh-edge.xml
 
-qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-cm.qcow2 40G
-virt-install --accelerate --name=pnda-cdh-cm --file=pnda-cdh-cm.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=32768 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-cm.xml
-virsh define pnda-cdh-cm.xml
+#qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-cm.qcow2 40G
+#virt-install --accelerate --name=pnda-cdh-cm --file=pnda-cdh-cm.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=32768 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-cm.xml
+#virsh define pnda-cdh-cm.xml
 
-for i in {1..3};do \
+for i in {1..1};do \
 qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-dn$i.qcow2 40G;\
 done
-for i in {1..3};do \
+for i in {1..1};do \
 virt-install --accelerate --name=pnda-cdh-dn$i --file=pnda-cdh-dn$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=8192 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-dn$i.xml; \
 virsh define pnda-cdh-dn$i.xml; \
 done
 
-for i in {1..2};do \
+for i in {1..1};do \
 qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-mgr$i.qcow2 40G;\
 done
-for i in {1..2};do \
+for i in {1..1};do \
 virt-install --accelerate --name=pnda-cdh-mgr$i --file=pnda-cdh-mgr$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=32768 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-mgr$i.xml; \
 virsh define pnda-cdh-mgr$i.xml; \
 done
