269a9601a01e754bdab3b429d618f16f6769fe72
Fabien Andrieux <krickwix@gmail.com>
2016-11-08 20:31:25 -0500

    Correcting rebase issues

--- baremetal/kvm_helpers/create_vms.sh
+++ baremetal/kvm_helpers/create_vms.sh
@@ -4,87 +4,22 @@ set -xe
 
 for i in $(virsh list --all | awk ' /pnda/ {print $2} '); do virsh undefine $i;done
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-# SALTMASTER
-qemu-img create -f qcow2 -o preallocation=metadata pnda-master.qcow2 10G
-<<<<<<< HEAD
-virt-install --accelerate --cpu SandyBridge,+vmx --name=pnda-master --file=pnda-master.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-master.xml
-<<<<<<< HEAD
-=======
 # SALTMASTER
 qemu-img create -f qcow2 -o preallocation=metadata pnda-master.qcow2 10G
 virt-install  --name=pnda-master --file=pnda-master.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-master.xml
->>>>>>> 2615de5ea61efb7cb992ab9efcc440454967f601
 virsh define pnda-master.xml
-
-# ZOOKEPER
-# for i in {1..1};do qemu-img create -f qcow2 -o preallocation=metadata pnda-zookeeper-$i.qcow2 20G;done
-# for i in {1..1};do virt-install --accelerate --name=pnda-zookeeper-$i --file=pnda-zookeeper-$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-zookeeper-$i.xml;done
-# for i in {1..1};do virsh define pnda-zookeeper-$i.xml; done
 
 # KAFKA
 for i in {1..1};do qemu-img create -f qcow2 -o preallocation=metadata pnda-kafka-$i.qcow2 40G;done
-<<<<<<< HEAD
-for i in {1..1};do virt-install --accelerate --cpu SandyBridge,+vmx --name=pnda-kafka-$i --file=pnda-kafka-$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-kafka-$i.xml;done
+for i in {1..1};do virt-install  --name=pnda-kafka-$i --file=pnda-kafka-$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-kafka-$i.xml;done
 for i in {1..1};do virsh define pnda-kafka-$i.xml; done
 
+# EDGE
 qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-edge.qcow2 20G
-virt-install --accelerate --cpu SandyBridge,+vmx --name=pnda-cdh-edge --file=pnda-cdh-edge.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch  --network network=default --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-edge.xml
+virt-install  --name=pnda-cdh-edge --file=pnda-cdh-edge.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=8192 --network bridge=br-ctlplane,virtualport_type=openvswitch  --network network=default --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-edge.xml
 virsh define pnda-cdh-edge.xml
 
-for i in {1..1};do \
-qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-dn$i.qcow2 40G;\
-done
-for i in {1..1};do \
-virt-install --accelerate --cpu SandyBridge,+vmx --name=pnda-cdh-dn$i --file=pnda-cdh-dn$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-dn$i.xml; \
-virsh define pnda-cdh-dn$i.xml; \
-done
-
-for i in {1..1};do \
-qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-mgr$i.qcow2 40G;\
-done
-for i in {1..1};do \
-<<<<<<< HEAD
-<<<<<<< HEAD
-virt-install --accelerate --cpu SandyBridge,+vmx --name=pnda-cdh-mgr$i --file=pnda-cdh-mgr$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-mgr$i.xml; \
-=======
-=======
-# SALTMASTER
->>>>>>> ngena
-qemu-img create -f qcow2 -o preallocation=metadata pnda-master.qcow2 10G
-virt-install --accelerate --name=pnda-master --file=pnda-master.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-master.xml
-=======
->>>>>>> update kvm_helpers
-=======
-virt-install  --name=pnda-master --file=pnda-master.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-master.xml
->>>>>>> machines sizes
-virsh define pnda-master.xml
-
-# ZOOKEPER
-#for i in {1..1};do qemu-img create -f qcow2 -o preallocation=metadata pnda-zookeeper-$i.qcow2 20G;done
-#for i in {1..1};do virt-install  --name=pnda-zookeeper-$i --file=pnda-zookeeper-$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-zookeeper-$i.xml;done
-#for i in {1..1};do virsh define pnda-zookeeper-$i.xml; done
-
-# KAFKA
-for i in {1..1};do qemu-img create -f qcow2 -o preallocation=metadata pnda-kafka-$i.qcow2 40G;done
-=======
->>>>>>> 2615de5ea61efb7cb992ab9efcc440454967f601
-for i in {1..1};do virt-install  --name=pnda-kafka-$i --file=pnda-kafka-$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=3184 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-kafka-$i.xml;done
-for i in {1..1};do virsh define pnda-kafka-$i.xml; done
-
-qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-edge.qcow2 20G
-virt-install --accelerate  --name=pnda-cdh-edge --file=pnda-cdh-edge.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=1 --ram=8192 --network bridge=br-ctlplane,virtualport_type=openvswitch  --network network=default --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-edge.xml
-virsh define pnda-cdh-edge.xml
-
-<<<<<<< HEAD
-#qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-cm.qcow2 40G
-#virt-install --accelerate --name=pnda-cdh-cm --file=pnda-cdh-cm.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-cm.xml
-#virsh define pnda-cdh-cm.xml
-
-=======
->>>>>>> 2615de5ea61efb7cb992ab9efcc440454967f601
+# DATANODES
 for i in {1..1};do \
 qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-dn$i.qcow2 40G;\
 done
@@ -93,30 +28,12 @@ virt-install  --name=pnda-cdh-dn$i --file=pnda-cdh-dn$i.qcow2 --graphics vnc,lis
 virsh define pnda-cdh-dn$i.xml; \
 done
 
+# MANAGER
 for i in {1..1};do \
 qemu-img create -f qcow2 -o preallocation=metadata pnda-cdh-mgr$i.qcow2 40G;\
 done
 for i in {1..1};do \
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-virt-install --accelerate --name=pnda-cdh-mgr$i --file=pnda-cdh-mgr$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=32768 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-mgr$i.xml; \
->>>>>>> adding baremetal directory
-=======
-virt-install --accelerate --cpu SandyBridge,+vmx --name=pnda-cdh-mgr$i --file=pnda-cdh-mgr$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-mgr$i.xml; \
->>>>>>> update kvm_helpers
-=======
-virt-install --accelerate --name=pnda-cdh-mgr$i --file=pnda-cdh-mgr$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=32768 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-mgr$i.xml; \
->>>>>>> kvm helpers for pico
-=======
-virt-install --accelerate --cpu SandyBridge,+vmx --name=pnda-cdh-mgr$i --file=pnda-cdh-mgr$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=2048 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-mgr$i.xml; \
->>>>>>> update kvm_helpers
-=======
-virt-install  --name=pnda-cdh-mgr$i --file=pnda-cdh-mgr$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=8192 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-mgr$i.xml; \
->>>>>>> machines sizes
-=======
-virt-install  --name=pnda-cdh-mgr$i --file=pnda-cdh-mgr$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=8192 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-mgr$i.xml; \
->>>>>>> 2615de5ea61efb7cb992ab9efcc440454967f601
+virt-install  --name=pnda-cdh-mgr$i --file=pnda-cdh-mgr$i.qcow2 --graphics vnc,listen=0.0.0.0 --vcpus=2 --ram=4096 --network bridge=br-ctlplane,virtualport_type=openvswitch,model=virtio  --network network=default,model=virtio --os-type=linux --boot hd --dry-run --print-xml > pnda-cdh-mgr$i.xml; \
 virsh define pnda-cdh-mgr$i.xml; \
 done
 
--- baremetal/kvm_helpers/tag_nodes.sh
+++ baremetal/kvm_helpers/tag_nodes.sh
@@ -1,43 +1,9 @@
 #!/bin/bash -v
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 2615de5ea61efb7cb992ab9efcc440454967f601
-for i in $(ironic node-list|grep -v UUID|grep zookeeper|awk {'print $2'});do ironic node-update $i add properties/capabilities=profile:zookeeper,boot_option:local;done
 for i in $(ironic node-list|grep -v UUID|grep kafka|awk {'print $2'});do ironic node-update $i add properties/capabilities=profile:kafka,boot_option:local;done
 for i in $(ironic node-list|grep -v UUID|grep master|awk {'print $2'});do ironic node-update $i add properties/capabilities=profile:master,boot_option:local;done
 for i in $(ironic node-list|grep -v UUID|grep edge|awk {'print $2'});do ironic node-update $i add properties/capabilities=profile:edge,boot_option:local;done
 for i in $(ironic node-list|grep -v UUID|grep dn|awk {'print $2'});do ironic node-update $i add properties/capabilities=profile:dn,boot_option:local;done
 for i in $(ironic node-list|grep -v UUID|grep mgr|awk {'print $2'});do ironic node-update $i add properties/capabilities=profile:mgr,boot_option:local;done
-<<<<<<< HEAD
-=======
-for i in $(ironic node-list|grep -v UUID|grep zookeeper|awk {'print $2'});do ironic node-update $i replace properties/capabilities=profile:zookeeper,boot_option:local;done
-for i in $(ironic node-list|grep -v UUID|grep kafka|awk {'print $2'});do ironic node-update $i replace properties/capabilities=profile:kafka,boot_option:local;done
-for i in $(ironic node-list|grep -v UUID|grep master|awk {'print $2'});do ironic node-update $i replace properties/capabilities=profile:master,boot_option:local;done
-for i in $(ironic node-list|grep -v UUID|grep tools|awk {'print $2'});do ironic node-update $i replace properties/capabilities=profile:tools,boot_option:local;done
-for i in $(ironic node-list|grep -v UUID|grep logserver|awk {'print $2'});do ironic node-update $i replace properties/capabilities=profile:logserver,boot_option:local;done
-for i in $(ironic node-list|grep -v UUID|grep edge|awk {'print $2'});do ironic node-update $i replace properties/capabilities=profile:edge,boot_option:local;done
-for i in $(ironic node-list|grep -v UUID|grep cm|awk {'print $2'});do ironic node-update $i replace properties/capabilities=profile:cm,boot_option:local;done
-for i in $(ironic node-list|grep -v UUID|grep dn|awk {'print $2'});do ironic node-update $i replace properties/capabilities=profile:dn,boot_option:local;done
-for i in $(ironic node-list|grep -v UUID|grep jupyter|awk {'print $2'});do ironic node-update $i replace properties/capabilities=profile:jupyter,boot_option:local;done
-for i in $(ironic node-list|grep -v UUID|grep mgr|awk {'print $2'});do ironic node-update $i replace properties/capabilities=profile:mgr,boot_option:local;done
-for i in $(ironic node-list|grep -v UUID|grep opentsdb|awk {'print $2'});do ironic node-update $i replace properties/capabilities=profile:opentsdb,boot_option:local;done
->>>>>>> adding baremetal directory
-=======
-for i in $(ironic node-list|grep -v UUID|grep zookeeper|awk {'print $2'});do ironic node-update $i add properties/capabilities=profile:zookeeper,boot_option:local;done
-for i in $(ironic node-list|grep -v UUID|grep kafka|awk {'print $2'});do ironic node-update $i add properties/capabilities=profile:kafka,boot_option:local;done
-for i in $(ironic node-list|grep -v UUID|grep master|awk {'print $2'});do ironic node-update $i add properties/capabilities=profile:master,boot_option:local;done
-for i in $(ironic node-list|grep -v UUID|grep edge|awk {'print $2'});do ironic node-update $i add properties/capabilities=profile:edge,boot_option:local;done
-for i in $(ironic node-list|grep -v UUID|grep dn|awk {'print $2'});do ironic node-update $i add properties/capabilities=profile:dn,boot_option:local;done
-for i in $(ironic node-list|grep -v UUID|grep mgr|awk {'print $2'});do ironic node-update $i add properties/capabilities=profile:mgr,boot_option:local;done
-<<<<<<< HEAD
-for i in $(ironic node-list|grep -v UUID|grep opentsdb|awk {'print $2'});do ironic node-update $i add properties/capabilities=profile:opentsdb,boot_option:local;done
->>>>>>> ngena
-=======
->>>>>>> kvm helpers for pico
-=======
->>>>>>> 2615de5ea61efb7cb992ab9efcc440454967f601
 
 for i in $(ironic node-list|grep -v UUID|awk {'print $2'});do ironic node-show $i|grep -A1 properties;done
